f473dd7f76024d9c74a3a2fe0d7cdd4e2ebcfed5
mykolashkolnuy-byte <mykolashkolnuy@gmail.com>
2026-07-29 00:53:28 +0300

    Update medicines.js

--- js/medicines.js
+++ js/medicines.js
@@ -1,1 +1,37 @@
+async function loadMedicines() {
 
+    const response = await fetch("data/medicines.json");
+
+    const medicines = await response.json();
+
+    const cards = document.querySelector(".cards");
+
+    cards.innerHTML = "";
+
+    medicines.forEach(medicine => {
+
+        cards.innerHTML += `
+            <div class="card">
+
+                <h3>${medicine.name}</h3>
+
+                <p>${medicine.description}</p>
+
+                <button onclick="openMedicine(${medicine.id})">
+                    Детальніше
+                </button>
+
+            </div>
+        `;
+
+    });
+
+}
+
+function openMedicine(id){
+
+    window.location.href = `medicine.html?id=${id}`;
+
+}
+
+loadMedicines();
