a2a6dffccc0b1a3a2013518681d9bd24717a26f1
mykolashkolnuy-byte <mykolashkolnuy@gmail.com>
2026-07-29 01:00:29 +0300

    Update medicine.js

--- js/medicine.js
+++ js/medicine.js
@@ -1,1 +1,41 @@
+async function loadMedicine() {
 
+    const params = new URLSearchParams(window.location.search);
+
+    const id = Number(params.get("id"));
+
+    const response = await fetch("data/medicines.json");
+
+    const medicines = await response.json();
+
+    const medicine = medicines.find(item => item.id === id);
+
+    if (!medicine) {
+
+        document.getElementById("medicine-name").textContent = "Препарат не знайдено";
+
+        return;
+
+    }
+
+    document.getElementById("medicine-name").textContent = medicine.name;
+
+    document.getElementById("medicine-description").textContent = medicine.description;
+
+    document.getElementById("ingredient").textContent = medicine.ingredient;
+
+    document.getElementById("category").textContent = medicine.category;
+
+    const list = document.getElementById("uses");
+
+    list.innerHTML = "";
+
+    medicine.uses.forEach(use => {
+
+        list.innerHTML += `<li>${use}</li>`;
+
+    });
+
+}
+
+loadMedicine();
